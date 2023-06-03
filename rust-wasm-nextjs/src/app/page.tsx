f5247e7889f3807e40f8421cf85bef83a085060c
James Logan <jlogan03@gmail.com>
2023-06-03 16:57:49 -0400

    add some commentary. add rust package to allow hosting on vercel.

--- rust-wasm-nextjs/src/app/page.tsx
+++ rust-wasm-nextjs/src/app/page.tsx
@@ -21,12 +21,21 @@ export default function Home() {
   const [show, changeShow] = useState(false);
 
   return (
-    <main className={styles.main}>
-      <div>
+    <main className={styles.description}>
+      <p>
+        Example client-side usage of Rust functions in NextJS via wasm-pack
+        following
+        https://javascript.plainenglish.io/next-js-webassembly-77df3537690 with
+        some adaptations for compatibility with Typescript and recent changes to
+        Next.
+      </p>
+      <p>Source: https://github.com/jlogan03/rust-wasm-nextjs</p>
+      <hr></hr>
+      <p>
         <button onClick={() => changeShow(true)}>Render WASM Component</button>
         Our WASM component:{" "}
         {show ? <WasmComponent /> : <p>is not rendered yet</p>}
-      </div>
+      </p>
     </main>
   );
 }
